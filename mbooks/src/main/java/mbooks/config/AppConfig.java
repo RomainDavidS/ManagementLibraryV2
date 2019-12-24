0e0dbe7265d33ac9b8ff2834ecc9db0105cdbb3f
Romain-David <romain-david.sergeant@enedis-grdf.fr>
2019-12-24 22:33:08 +0100

    modification properies email de mbooks

--- mbooks/src/main/java/mbooks/config/AppConfig.java
+++ mbooks/src/main/java/mbooks/config/AppConfig.java
@@ -2,39 +2,19 @@ package mbooks.config;
 
 
 import mbooks.technical.date.SimpleDate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @Configuration
 public class AppConfig {
+
+
 
     @Bean
     public SimpleDate simpleDate(){ return  new SimpleDate(); }
 
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost( "${spring.mail.host}" );
-        mailSender.setPort( Integer.valueOf( "${spring.mail.port}" ));
-
-        mailSender.setUsername( "${spring.mail.username}" );
-        mailSender.setPassword( "${spring.mail.password}" );
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp" );
-        props.put("mail.smtp.auth", true );
-        props.put("mail.smtp.starttls.enable", true );
-        props.put("mail.debug", true );
-
-        return mailSender;
-    }
 
     @Bean
     public SimpleMailMessage templateSimpleMessage() {
