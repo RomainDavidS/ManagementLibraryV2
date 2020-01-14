d636769f16e4fa5e15a67a18b65bbf8c7de746a7
RomainDavidS <46599126+RomainDavidS@users.noreply.github.com>
2020-01-14 05:27:04 +0100

    Update data.sql

--- musers/src/main/resources/data.sql
+++ musers/src/main/resources/data.sql
@@ -11,13 +11,13 @@ VALUES
 INSERT INTO "users"
 (id,email,active,last_name,password,first_name,phone)
 VALUES
-(1,'romaindavid.sergeant@gmail.com',
+(1,'user1',
 true,'USER','$2a$10$mVLweGs6HLxItvuYt5W21e9sr7sgkqeuk6q.3pke4HEHJZry4fWSO',
 'Romain-David' ,'0768000001'),
-(2,'romaind.sergeant@gmail.com',
+(2,'user2',
 true,'ACTUATOR','$2a$10$mVLweGs6HLxItvuYt5W21e9sr7sgkqeuk6q.3pke4HEHJZry4fWSO',
 'RomainD' ,'0768000002'),
-(3,'romaind.ocrlibrary@gmail.com',
+(3,'user3',
 true,'ADMIN','$2a$10$mVLweGs6HLxItvuYt5W21e9sr7sgkqeuk6q.3pke4HEHJZry4fWSO',
 'Library Admin' ,'0768000003');
 
